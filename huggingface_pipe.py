64924b153268d11fee82dddccb5e961da46ee593
a100-dev-1 <adbukashkin@sberbank.ru>
2024-05-28 11:27:51 +0000

    rename

---
+++ huggingface_pipe.py
@@ -0,0 +1,41 @@
+import torch
+from omegaconf import OmegaConf
+from diffusers import StableDiffusionXLInpaintPipeline, AutoPipelineForInpainting
+from sam_stable_test.inpainting_pipe import InpaintingPipeline
+
+
+class HuggingfacePipeline(InpaintingPipeline):
+
+    def __init__(self, model_name):
+        super().__init__()
+
+        self.conf = OmegaConf.load("config.yaml")
+        
+        self.pipe = self.load_pipe(model_name)
+        self.pipe.to(self.device)
+
+    def load_pipe(self, model_name):
+        conf = self.conf[model_name]
+
+        dtype_map = {
+            "fp16": torch.float16,
+            "fp32": torch.float32
+        }
+
+        torch_dtype_str = conf["torch_dtype"]
+        torch_dtype = dtype_map[torch_dtype_str]
+
+        pipe_class_str = conf["class"]
+        pipe_class = eval(pipe_class_str)
+
+        pipe = pipe_class.from_pretrained(
+            conf["checkpoint"],
+            torch_dtype=torch_dtype,
+            use_safetensors=conf["use_safetensors"]
+        )
+        return pipe
+
+
+if __name__ == "__main__":
+    pipe = HuggingfacePipeline("kandi21")
+
--- inpainting_pipe.py
+++ inpainting_pipe.py
@@ -1,41 +1,17 @@
+from abc import ABC, abstractmethod
 import torch
-from omegaconf import OmegaConf
-from diffusers import StableDiffusionXLInpaintPipeline, AutoPipelineForInpainting
-from abs_inpainting import InpaintingPipeline
 
 
-class HuggingfacePipeline(InpaintingPipeline):
+class InpaintingPipeline(ABC):
+    def __init__(
+        self,
+    ):
+        device_str = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(device_str)
 
-    def __init__(self, model_name):
-        super().__init__()
-
-        self.conf = OmegaConf.load("config.yaml")
-        
-        self.pipe = self.load_pipe(model_name)
-        self.pipe.to(self.device)
-
-    def load_pipe(self, model_name):
-        conf = self.conf[model_name]
-
-        dtype_map = {
-            "fp16": torch.float16,
-            "fp32": torch.float32
-        }
-
-        torch_dtype_str = conf["torch_dtype"]
-        torch_dtype = dtype_map[torch_dtype_str]
-
-        pipe_class_str = conf["class"]
-        pipe_class = eval(pipe_class_str)
-
-        pipe = pipe_class.from_pretrained(
-            conf["checkpoint"],
-            torch_dtype=torch_dtype,
-            use_safetensors=conf["use_safetensors"]
-        )
-        return pipe
+    @abstractmethod
+    def load_pipe(self):
+        pass
 
 
-if __name__ == "__main__":
-    pipe = HuggingfacePipeline("kandi21")
 
